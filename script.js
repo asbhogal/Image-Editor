2fd97201f5c2455acb92855548f379c69e516466
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-07-28 10:59:31 +0100

    amendments

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const fileInput = document.querySelector(".file-input"),
 chooseImgBtn = document.querySelector(".choose-img");
 
+const loadImage = () => {
+    let file = fileInput.files[0];
+    console.log(file);
+}
+
+fileInput.addEventListener("change", loadImage);
 chooseImgBtn.addEventListener("click", () => fileInput.click());
