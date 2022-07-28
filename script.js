cce4355138e1e557a2d6d6b86f1d12d2b311974b
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2022-07-28 11:41:00 +0100

    additional code

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const fileInput = document.querySelector(".file-input"),
+previewImg = document.querySelector(".preview-img img");
 chooseImgBtn = document.querySelector(".choose-img");
 
 const loadImage = () => {
     let file = fileInput.files[0];
+    if(!file) return;
+    previewImg.src = URL.createObjectURL(file);
     console.log(file);
 }
 
